a5e0d5fb6f6af2f506daa2beeb97a6ae14a156cd
Jasson189 <mazariegosvinicio4@gmail.com>
2026-04-18 00:30:08 -0600

    Initial commit

---
+++ backend/backend/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres123@127.0.0.1:5432/remesas"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
